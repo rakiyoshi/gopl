63981763ea96a74ec63ffbc03c5f49f8b7b1ed75
Ryoh Akiyoshi <ryo614@gmail.com>
2022-05-28 12:24:48 +0900

    WIP

---
+++ chapter02/tempconv0/main.go
@@ -0,0 +1,13 @@
+// tempconv package calculates temperatures of the Celsius and Fahrenheit
+package main
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+for
