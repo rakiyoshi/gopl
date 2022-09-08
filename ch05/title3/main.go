eef5dac86ca4061c724873b7aeb982eb1258b498
Ryoh Akiyoshi <ryo614@gmail.com>
2022-09-09 01:53:16 +0900

    5.10: recover

---
+++ ch05/title3/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+		case bailout{}:
+			// EXPECTED panic
+			err = fmt.Errorf("multiple title elements")
+		default:
+			// unexpected (normal) panic. keep panic
+			panic(p)
+		}
+	}()
+
+	forEachNode(
+		doc,
+		func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+				if title != "" {
+					panic(bailout{}) // doc has multiple title
+				}
+				title = n.FirstChild.Data
+			}
+		},
+		nil,
+	)
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+	return title, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+func main() {
+	title(os.Args[1])
+}
