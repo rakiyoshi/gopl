c2cd35af3d7c10fd38cf5e6d2f62afe954aaab9f
Ryoh Akiyoshi <ryo614@gmail.com>
2022-09-09 02:01:47 +0900

    fix lint

--- ch05/title3/main.go
+++ ch05/title3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"golang.org/x/net/html"
@@ -54,6 +55,33 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	}
 }
 
+func getURL(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return doc, nil
+}
+
 func main() {
-	title(os.Args[1])
+	doc, err := getURL(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+	title, err := soleTitle(doc)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+	fmt.Println(title)
 }
